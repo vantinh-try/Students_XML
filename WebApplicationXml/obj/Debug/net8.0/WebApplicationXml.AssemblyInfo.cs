1d11e498dbd3ceb2a8400259dfe9176defc53392
Văn Tịnh <vantinh@VanTinh.local>
2024-09-24 19:49:14 +0700

    Cập nhật docker3

--- WebApplicationXml/obj/Debug/net8.0/WebApplicationXml.AssemblyInfo.cs
+++ WebApplicationXml/obj/Debug/net8.0/WebApplicationXml.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplicationXml")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb0e0d269396e37324c135235e9bbaa53b7bd69b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3212c0eb631f0e4b595d50357f385aaf16b57c0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplicationXml")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplicationXml")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
